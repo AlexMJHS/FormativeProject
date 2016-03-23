319d6534635285ab1f6e38a4d52eca9e87ec2f34
Alejandro <AlexMJHS@gmail.com>
2016-03-23 08:31:34 -0600

    Added FormativeFrame

--- formative/view/FormativeFrame.java
+++ formative/view/FormativeFrame.java
@@ -1,6 +1,32 @@
 package formative.view;
 
-public class FormativeFrame
-{
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import formative.controller.FormativeController;
 
+public class FormativeFrame extends JFrame
+{
+	private FormativeController baseController;
+	private FormativePanel basePanel;
+	
+	public FormativeFrame(FormativeController baseController)
+	{
+		this.baseController = baseController;
+		basePanel = new FormativePanel(baseController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setSize(500,500);
+		this.setTitle("Formative Project");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	public FormativeController getBaseController()
+	{
+		return baseController;
+	}
 }
