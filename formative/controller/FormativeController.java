2583602f7ff8c2472cc2ea51401640ec9182284d
Alejandro <AlexMJHS@gmail.com>
2016-03-23 08:01:24 -0600

    Add to repo

---
+++ formative/controller/FormativeController.java
@@ -0,0 +1,55 @@
+package formative.controller;
+
+
+/**
+ * 
+ * @author Alejandro Magallanes
+ *@version 0.1 3/23/16
+ */
+public class FormativeController
+{
+	
+	
+	
+	public FormativeController()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+	private void chat()
+	{
+		
+	}
+	
+	public String userToChatBot(String conversation)
+	{
+		
+	}
+	
+	private void shutDown()
+	{
+		
+	}
+	
+	public Chatbot getChatbot()
+	{
+		return myBot;
+	}
+	
+	public ChatView getChatView()
+	{
+		return myDisplay;
+	}
+	
+	public ChatFrame getBaseFrame()
+	{
+		return baseFrame;
+	}
+	
+	
+}
---
+++ formative/controller/FormativeRunner.java
@@ -0,0 +1,12 @@
+package formative.controller;
+
+import formative.controller.FormativeController;
+
+public class FormativeRunner
+{
+	public static void main(String [] args)
+	{
+		FormativeController myFormativeController = new FormativeController();
+		myFormativeController.start();
+	}
+}
